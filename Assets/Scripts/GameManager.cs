b9ad3c91b830d4c8fdec2ceb6048b82ad7ef6732
h3nrey <pedro.novaes.education@gmail.com>
2023-05-19 16:54:16 -0300

    adding score text and won container

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public static class GameSounds {
     public static readonly string reflect = "reflect";
@@ -10,13 +12,54 @@ public static class GameSounds {
 public class GameManager : MonoBehaviour
 {
     public static GameManager game;
-    [SerializeField] private Sound[] sounds;
-    [ReadOnly] public List<AudioSource> audioSrcs;
     [SerializeField] private GameObject ball;
 
+    [Header("UI")]
+    [SerializeField] TMP_Text livesText;
+    [SerializeField] GameObject WinContainer;
+
+    [Header("AUDIO")]
+    [SerializeField] private Sound[] sounds;
+    [ReadOnly] public List<AudioSource> audioSrcs;
+
+
+    private void Awake() {
+        game = this;
+
+    }
+
+    private void Start() {
+        livesText.text = PlayerController.instance.currentTries.ToString();
+    }
+
+    private void LateUpdate() {
+        livesText.text = PlayerController.instance.currentTries.ToString();
+    }
+
+    public void RestartGame() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoToNextLevel() {
+        Time.timeScale = 1;
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex + 1);
+    }
+    public void EndGame() {
+        Time.timeScale = 0;
+    }
+
+    public void LevelWon() {
+        WinContainer.SetActive(true);
+        Destroy(ball);
+        Time.timeScale = 0;
+    }
+
+    #region Sound
     [Button("Populate Sound")]
     public void PopulateSounds() {
-        foreach(Sound s in sounds) {
+        foreach (Sound s in sounds) {
             AudioSource newAudioSource = gameObject.AddComponent<AudioSource>();
             s.source = newAudioSource;
             audioSrcs.Add(newAudioSource);
@@ -25,26 +68,13 @@ public class GameManager : MonoBehaviour
             s.source.playOnAwake = s.playOnAwake;
         }
     }
+   
     [Button("Clear Sounds")]
     public void ClearSound() {
-        foreach(AudioSource src in audioSrcs) {
+        foreach (AudioSource src in audioSrcs) {
             DestroyImmediate(src);
         }
         audioSrcs.Clear();
-    }
-
-    private void Awake() {
-        game = this;
-
-    }
-    public void EndGame() {
-        Time.timeScale = 0;
-    }
-
-    public void LevelWon() {
-        print("won");
-        Destroy(ball);
-        Time.timeScale = 0;
     }
 
     public void playSound(string name) {
@@ -63,4 +93,5 @@ public class GameManager : MonoBehaviour
             return;
         }
     }
+    #endregion
 }
